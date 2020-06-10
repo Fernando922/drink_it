b01f003ce102b352b0653d435a1db4735f5dd199
Fernando de Paula <dipaula018@gmail.com>
2020-06-10 01:37:23 -0300

    feat: filter by category

--- src/pages/drinks/index.js
+++ src/pages/drinks/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useRoute } from '@react-navigation/native';
 import { FlatList } from 'react-native';
 import { Container } from './styles';
 import DrinkCard from '../../components/drinkCard';
@@ -8,17 +9,24 @@ import api from '../../services/api';
 export default function Drinks() {
   const [loading, setLoading] = useState(true);
   const [drinks, setDrinks] = useState([]);
+  const route = useRoute();
 
   useEffect(() => {
+    setLoading(true);
     function findDrinks() {
+      let query = 'a=Alcoholic';
+
+      if (route.params) {
+        query = route.params.query;
+      }
       return api
-        .get(`/filter.php?a=Alcoholic`)
+        .get(`/filter.php?${query}`)
         .then((res) => setDrinks(res.data.drinks))
         .then(() => setLoading(false));
     }
 
     findDrinks();
-  }, []);
+  }, [route]);
 
   return (
     <Loading active={loading}>
